1f82d0d26f6dc18176840e4f14b742c47ed5e0b5
wydler <michael.wydler@gmail.com>
2012-11-09 22:53:19 +0100

    Iterative Tiefensuche implementiert. Tiefen- und Breitensuche verbessert.

--- 8puzzle.py
+++ 8puzzle.py
@@ -1,163 +1,172 @@
+import copy
 from Tkinter import *
 
-state = ['1', '3', '0', '5', '2', '6', '4', '7', '8']
+state = {"depth" : 0, "values" : ['1', '3', '0', '5', '2', '6', '4', '7', '8'], "parent" : None}
 goal = ['1', '2', '3', '4', '5', '6', '7', '8', '0']
 moves = 0
 
-def update():
+def update(state):
 	for i,v in enumerate(state):
 		if v == '0':
 			Label(root, text="-").grid(row=i-i%3, column=i%3, padx=10, pady=10)
 		else:
 			Label(root, text=v).grid(row=i-i%3, column=i%3, padx=10, pady=10)
 
-def move(direction):
+def move(state, direction):
 	i = state.index('0')
 
 	if direction == 'Left':
 		if i%3 != 0:
-			swap(i, i-1)
+			swap(state, i, i-1)
 		else:
 			return False
 
 	if direction == 'Right':
 		if i%3 != 2:
-			swap(i, i+1)
+			swap(state, i, i+1)
 		else:
 			return False
 
 	if direction == 'Up':
 		if i > 2:
-			swap(i, i-3)
+			swap(state, i, i-3)
 		else:
 			return False
 
 	if direction == 'Down':
 		if i < 6:
-			swap(i, i+3)
+			swap(state, i, i+3)
 		else:
 			return False
 
-	update()
-	if is_done():
+	update(state)
+	if is_done(state):
 		print "SOLVED!"
 	return True
 
-def swap(i, j):
+def swap(state, i, j):
 	state[i], state[j] = state[j], state[i]
 	return state
 
-def is_done():
+def is_done(state):
 	if state == goal:
 		return True
 
 def keypress(event):	
 	if event.keysym == 'Left':
-		move('Left')
+		move(state['values'], 'Left')
 	if event.keysym == 'Right':
-		move('Right')
+		move(state['values'],'Right')
 	if event.keysym == 'Up':
-		move('Up')
+		move(state['values'],'Up')
 	if event.keysym == 'Down':
-		move('Down')
+		move(state['values'],'Down')
 
 	if event.char == 'b':
 		print "Breitensuche"
-		BFS(state).start()
+		BFS().solve(state)
 	if event.char == 'd':
 		print "Tiefensuche"
-		DFS(state).start()
+		DFS().solve(state)
+	if event.char == 'i':
+		print "Iterative Tiefensuche"
+		IDDFS().solve(state)
 
 class DFS:
-	state = []
+	def solve(self, state):
+		if state == goal:
+			print 'SOLVED!'
+			print state
+			return state
 
-	def __init__(self, state):
-		self.state = state
-
-	def start(self):
-		if self.state == goal:
-			print self.state
-			return self.state
-		else:
-			nodes = self.nodes(self.state)
-			for node in nodes:
-				return DFS(node).start()	
-
-	def nodes(self, current):
-		i = current.index('0')
-		nodes = []
-
-		if i%3 != 0:
-			tmp = current[:]
-			tmp[i], tmp[i+1] = tmp[i+1], tmp[i]
-			nodes.append(tmp)
-		if i%3 != 2:
-			tmp = current[:]
-			tmp[i], tmp[i-1] = tmp[i-1], tmp[i]
-			nodes.append(tmp)
-		if i > 2:
-			tmp = current[:]
-			tmp[i], tmp[i-3] = tmp[i-3], tmp[i]
-			nodes.append(tmp)
-		if i < 6:
-			tmp = current[:]
-			tmp[i], tmp[i+3] = tmp[i+3], tmp[i]
-			nodes.append(tmp)
-
-		return nodes
+		for node in expand(state):
+			return DFS().solve(node)
 
 class BFS:
 	queue = []
 	visited = []
 
-	def __init__(self, state):
-		self.queue.append(state)
+	def solve(self, state):
+		self.queue = [state]
+		self.visited = []
 
-	def start(self):
 		while len(self.queue) > 0:
 			active = self.queue.pop(0)
-			self.visited.append(active)
+			self.visited.append(active['values'])
 
-			nodes = self.nodes(active)
+			nodes = expand(active)
 			if len(nodes) > 0:
 				for node in nodes:
-					if node in self.visited:
+					if node['values'] in self.visited:
 						continue
-					if node == goal:
-						print 'SOLVED!' 
+					if node['values'] == goal:
+						print 'SOLVED!'
 						print node
 						return True
 					self.queue.append(node)
 
 		return False
 
-	def nodes(self, current):
-		i = current.index('0')
-		nodes = []
+class IDDFS:
+	def solve(self, node):
+		depth = 0
+		solution = None
 
-		if i%3 != 0:
-			tmp = current[:]
-			tmp[i], tmp[i-1] = tmp[i-1], tmp[i]
-			nodes.append(tmp)
-		if i%3 != 2:
-			tmp = current[:]
-			tmp[i], tmp[i+1] = tmp[i+1], tmp[i]
-			nodes.append(tmp)
-		if i > 2:
-			tmp = current[:]
-			tmp[i], tmp[i-3] = tmp[i-3], tmp[i]
-			nodes.append(tmp)
-		if i < 6:
-			tmp = current[:]
-			tmp[i], tmp[i+3] = tmp[i+3], tmp[i]
-			nodes.append(tmp)
+		while not solution:
+			solution = DLS().solve(node, depth)
+			depth = depth + 1
 
-		return nodes
+		return solution
+			
+class DLS:
+	def solve(self, state, depth):
+		#print state
+		if state['values'] == goal:
+			print "SOLVED \n" + str(state)
+			return state
 
+		for node in expand(state):
+			if node['depth'] < depth:
+				result = DLS().solve(node, depth);
+				if result:
+					return result
+
+		return None
+
+def expand(node):
+	i = node['values'].index('0')
+	nodes = []
+
+	if i%3 != 0:
+		tmp = copy.deepcopy(node)
+		tmp['depth'] = node['depth'] + 1
+		tmp['values'][i], tmp['values'][i-1] = tmp['values'][i-1], tmp['values'][i]
+		tmp['parent'] = node
+		nodes.append(tmp)
+	if i%3 != 2:
+		tmp = copy.deepcopy(node)
+		tmp['depth'] = node['depth'] + 1
+		tmp['values'][i], tmp['values'][i+1] = tmp['values'][i+1], tmp['values'][i]
+		tmp['parent'] = node
+		nodes.append(tmp)
+	if i > 2:
+		tmp = copy.deepcopy(node)
+		tmp['depth'] = node['depth'] + 1
+		tmp['values'][i], tmp['values'][i-3] = tmp['values'][i-3], tmp['values'][i]
+		tmp['parent'] = node
+		nodes.append(tmp)
+	if i < 6:
+		tmp = copy.deepcopy(node)
+		tmp['depth'] = node['depth'] + 1
+		tmp['values'][i], tmp['values'][i+3] = tmp['values'][i+3], tmp['values'][i]
+		tmp['parent'] = node
+		nodes.append(tmp)
+
+	return nodes
 
 root = Tk()
 root.bind_all('<Key>', keypress)
 
-update()
+update(state['values'])
 
 root.mainloop()
